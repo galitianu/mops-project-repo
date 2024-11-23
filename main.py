05dfb66b7b9202f5425b851389b3e4e15d12be5a
Andrei Galitianu <andrei@galitianu.com>
2024-11-23 11:21:14 +0200

    some other modifications

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
-    print("Hello!")
-    print("This function does things")
-    if(1>3):
-        print("You will never see this")
+    print("Hallo!")
+    print(123)
+    if(1<3):
+        print("You will always see this")
         
 main()
